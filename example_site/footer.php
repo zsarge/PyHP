1ce749e179ca5b15d58df75aa5267d87491365d4
Zack Sargent <zackarywsargent@gmail.com>
2023-05-26 00:01:02 -0400

    change footer design

--- example_site/footer.php
+++ example_site/footer.php
@@ -1,2 +1,5 @@
 {% from datetime import datetime %}
-{{ f'Copyright {datetime.now().strftime("%Y")} {author_name}'}}
+
+<footer>
+    {{ f'&copy; {datetime.now().strftime("%Y")} {author_name}'}}
+</footer>
